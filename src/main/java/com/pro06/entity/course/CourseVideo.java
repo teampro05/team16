b1767d93abc0b066b7ff1b14404829b84c4d2273
sedoll <do11anm@naver.com>
2023-12-22 10:49:47 +0900

    [oh 231222] 강좌 ot 영상 추가

---
+++ src/main/java/com/pro06/entity/course/CourseVideo.java
@@ -0,0 +1,51 @@
+package com.pro06.entity.course;
+
+import com.pro06.dto.course.CourseVideoDto;
+import com.pro06.dto.course.VideoDto;
+import com.pro06.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+// 강의 ot 영상
+// 강의 ot 영상 정보를 저장
+
+@Entity
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+// column에 defualt 값을 설정할 때에 밑의 두개를 같이 써줘야 한다.
+@DynamicInsert
+@DynamicUpdate
+public class CourseVideo extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer no;             // 영상번호
+
+    @Column(nullable = false)
+    private String savefolder;      // 저장경로
+
+    @Column(nullable = false)
+    private String originfile;      // 실제 파일 이름
+
+    @Column(nullable = false)
+    private String savefile;        // 저장된 파일 이름
+
+    @Column(nullable = false)
+    private Long filesize;          // 파일 크기
+
+    @ManyToOne(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    @JoinColumn(name = "cno", referencedColumnName = "no")
+    private Course course;        // 강좌 번호 외래키 지정
+
+    // video 정보 수정에 필요한 메소드
+    public void change(CourseVideoDto dto) {
+        this.originfile = dto.getOriginfile();
+        this.savefile = dto.getSavefile();
+        this.filesize = dto.getFilesize();
+    }
+}
